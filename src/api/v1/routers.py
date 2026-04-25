ba69698d7dc98971023b0f9db046b3b409436d2a
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2026-04-25 15:13:02 +0530

    feat: razorpay feature include

--- src/api/v1/routers.py
+++ src/api/v1/routers.py
@@ -1,28 +1,35 @@
 from fastapi import APIRouter
 
 # Import all endpoint routers
-from src.api.v1.endpoints import auth, bookings, rooms
+from src.api.v1.endpoints import auth, bookings, rooms, pay
 
 # Create main router for v1
 api_router = APIRouter()
 
-# Include individual route modules
+# Auth
 api_router.include_router(
     auth.router,
     prefix="/auth",
     tags=["Auth"]
 )
 
-# Include individual route modules
+# Bookings
 api_router.include_router(
     bookings.router,
     prefix="/bookings",
     tags=["Bookings"]
 )
 
-# Include individual route modules
+# Rooms
 api_router.include_router(
     rooms.router,
     prefix="/rooms",
     tags=["Rooms"]
+)
+
+# Payment
+api_router.include_router(
+    pay.router,
+    prefix="/pay",
+    tags=["Payment"]
 )
